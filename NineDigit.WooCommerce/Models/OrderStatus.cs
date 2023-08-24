7472edaa792e097133c8a603508d2ad01e5a4799
yurislav <juraj.malach@gmail.com>
2023-08-24 09:41:38 +0200

    xml comments added

--- NineDigit.WooCommerce/Models/OrderStatus.cs
+++ NineDigit.WooCommerce/Models/OrderStatus.cs
@@ -6,18 +6,44 @@ public enum OrderStatus
 {
     [Description("any")]
     Any,
+    /// <summary>
+    /// Order received, no payment initiated. Awaiting payment (unpaid).
+    /// </summary>
     [Description("pending")]
     Pending,
+    /// <summary>
+    /// Payment received (paid) and stock has been reduced; order is awaiting fulfillment.
+    /// All product orders require processing, except those that only contain products
+    /// which are both virtual and downloadable.
+    /// </summary>
     [Description("processing")]
     Processing,
+    /// <summary>
+    /// Awaiting payment – stock is reduced, but you need to confirm payment.
+    /// </summary>
     [Description("on-hold")]
     OnHold,
+    /// <summary>
+    /// Order fulfilled and complete – requires no further action.
+    /// </summary>
     [Description("completed")]
     Completed,
+    /// <summary>
+    /// Cancelled by an admin or the customer – stock is increased, no further action required.
+    /// Please note: This status does not refund the customer.
+    /// </summary>
     [Description("cancelled")]
     Cancelled,
+    /// <summary>
+    /// Refunded by an admin – no further action required.
+    /// </summary>
     [Description("refunded")]
     Refunded,
+    /// <summary>
+    /// Payment failed or was declined (unpaid) or requires authentication (SCA).
+    /// Note that this status may not show immediately
+    /// and instead show as Pending until verified (e.g., PayPal).
+    /// </summary>
     [Description("failed")]
     Failed,
     [Description("trash")]
--- NineDigit.WooCommerce/Models/ProductStatus.cs
+++ NineDigit.WooCommerce/Models/ProductStatus.cs
@@ -2,16 +2,31 @@
 
 namespace NineDigit.WooCommerce;
 
+/// <summary>
+/// Product status is a way of indicating whether a product is available for purchase or not.
+/// </summary>
 public enum ProductStatus
 {
     [Description("any")]
     Any,
+    /// <summary>
+    /// The product is not yet available for purchase, but it is in development.
+    /// </summary>
     [Description("draft")]
     Draft,
+    /// <summary>
+    /// This means the product is awaiting approval from a WooCommerce administrator.
+    /// </summary>
     [Description("pending")]
     Pending,
+    /// <summary>
+    /// This means the product is only available for purchase by certain users (e.g., members of a private club).
+    /// </summary>
     [Description("private")]
     Private,
+    /// <summary>
+    /// The product is available for purchase.
+    /// </summary>
     [Description("publish")]
-    Publish
+    Published
 }

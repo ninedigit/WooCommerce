84bf419430ffb2c9dba8cc28667219ebb3967654
yurislav <juraj.malach@gmail.com>
2023-08-24 10:49:16 +0200

    auto postifx in options url implemented

---
+++ NineDigit.WooCommerce/Extensions/UriExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace System;
+
+internal static class UriExtensions
+{
+    public static Uri EnsurePostfix(this Uri self, string postfix)
+    {
+        if (!self.AbsolutePath.EndsWith(postfix, StringComparison.OrdinalIgnoreCase))
+        {
+            var newPath = self.AbsolutePath.TrimEnd('/') + postfix;
+            return new UriBuilder(self) { Path = newPath }.Uri;
+        }
+
+        return self;
+    }
+}
--- NineDigit.WooCommerce/WooCommerceApiClientOptions.cs
+++ NineDigit.WooCommerce/WooCommerceApiClientOptions.cs
@@ -4,6 +4,14 @@ namespace NineDigit.WooCommerce;
 
 public sealed class WooCommerceApiClientOptions
 {
+    const string WooCommerceApiV3Postfix = "/wp-json/wc/v3";
+
+    /// <summary>
+    /// </summary>
+    /// <param name="url">The WooCommerce webpage URL. E.g. <c>https://foo.bar</c> or <c>https://foo.bar/wp-json/wc/v3</c></param>
+    /// <param name="publicKey">The public key</param>
+    /// <param name="privateKey">The private key (secret)</param>
+    /// <exception cref="ArgumentException"></exception>
     public WooCommerceApiClientOptions(Uri url, string publicKey, string privateKey)
     {
         Guard.IsNotNull(url);
@@ -20,7 +28,7 @@ public sealed class WooCommerceApiClientOptions
         if (string.IsNullOrWhiteSpace(privateKey))
             throw new ArgumentException($"'{nameof(privateKey)}' cannot be null or whitespace.", nameof(privateKey));
 
-        Url = url;
+        Url = url.EnsurePostfix(WooCommerceApiV3Postfix);
         PublicKey = publicKey;
         PrivateKey = privateKey;
     }

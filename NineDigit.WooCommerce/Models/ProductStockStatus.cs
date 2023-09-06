aa9a3a35cd952e1bc2c5e2af68c49ed93c23627c
yurislav <juraj.malach@gmail.com>
2023-09-06 11:16:52 +0200

    xml comments added.

--- NineDigit.WooCommerce/Models/ProductStockStatus.cs
+++ NineDigit.WooCommerce/Models/ProductStockStatus.cs
@@ -11,9 +11,18 @@ public sealed class ProductStockStatus
     {
         this.value = value;
     }
-
+    
+    /// <summary>
+    /// In stock.
+    /// </summary>
     public static ProductStockStatus InStock { get; } = new ProductStockStatus("instock");
+    /// <summary>
+    /// Out of stock.
+    /// </summary>
     public static ProductStockStatus OutOfStock { get; } = new ProductStockStatus("outofstock");
+    /// <summary>
+    /// On back order
+    /// </summary>
     public static ProductStockStatus OnBackOrder { get; } = new ProductStockStatus("onbackorder");
 
     public static implicit operator string(ProductStockStatus status)

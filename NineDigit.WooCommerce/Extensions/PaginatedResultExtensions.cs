3a1cfd2823af58b6e961fff9739b6953933d862a
Juraj Malach <juraj.malach@gmail.com>
2024-11-14 09:08:21 +0100

    new extension method added

---
+++ NineDigit.WooCommerce/Extensions/PaginatedResultExtensions.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace NineDigit.WooCommerce;
+
+internal static class PaginationExtensions
+{
+    public static bool IsLastPage<T>(this Pagination pagination, PaginatedResult<T> paginatedResult)
+    {
+        Guard.IsNotNull(pagination);
+        Guard.IsNotNull(paginatedResult);
+
+        return pagination.Skip + paginatedResult.Count == paginatedResult.TotalCount;
+    }
+
+    public static Pagination NextPage(this Pagination pagination)
+    {
+        Guard.IsNotNull(pagination);
+
+        int? skip = (!pagination.Skip.HasValue && !pagination.Take.HasValue)
+            ? null
+            : (pagination.Skip ?? 0) + (pagination.Take ?? 0);
+
+        return new Pagination
+        {
+            Skip = skip,
+            Take = pagination.Take
+        };
+    }
+
+    public static bool TryGetNextPage<T>(
+        this Pagination self,
+        PaginatedResult<T> paginatedResult,
+        [NotNullWhen(true)] out Pagination? pagination)
+    {
+        Guard.IsNotNull(self);
+        Guard.IsNotNull(paginatedResult);
+
+        if (IsLastPage(self, paginatedResult))
+            pagination = null;
+        else
+            pagination = new Pagination
+            {
+                Skip = (self.Skip ?? 0) + (self.Take ?? paginatedResult.Count),
+                Take = self.Take
+            };
+
+        return pagination is not null;
+    }
+}
--- NineDigit.WooCommerce/Extensions/WooCommerceApiClientExtensions.cs
+++ NineDigit.WooCommerce/Extensions/WooCommerceApiClientExtensions.cs
@@ -54,6 +54,50 @@ public static class WooCommerceApiClientExtensions
         return result;
     }
 
+    public static async Task<PaginatedResult<Order>> GetAllOrdersAsync(
+        this WooCommerceApiClient self,
+        OrdersQuery query,
+        CancellationToken cancellationToken = default)
+    {
+        var originalPagination = query.Pagination ?? new Pagination { Skip = 0, Take = null };
+
+        if (!originalPagination.Take.HasValue)
+        {
+            originalPagination = new Pagination
+            {
+                Skip = originalPagination.Skip,
+                Take = Pagination.MaxTakeValue
+            };
+        }
+
+        query = new OrdersQuery
+        {
+            Pagination = originalPagination,
+            SortDirection = query.SortDirection,
+            SortBy = query.SortBy,
+            Filter = query.Filter
+        };
+
+        var paginatedResults = new List<PaginatedResult<Order>>();
+
+        for (var i = 0; ; i++)
+        {
+            var paginatedResult = await self.GetOrdersAsync(query, cancellationToken).ConfigureAwait(false);
+
+            paginatedResults.Add(paginatedResult);
+
+            if (!query.Pagination.TryGetNextPage(paginatedResult, out var nextPage))
+                break;
+
+            query.Pagination = nextPage;
+        }
+
+        var mergedItems = paginatedResults.SelectMany(i => i.Items).ToList();
+        var result = new PaginatedResult<Order>(mergedItems, paginatedResults.Last().TotalCount);
+
+        return result;
+    }
+
     /// <summary>
     /// Gets all orders (executes multiple request and fetches all pages).
     /// </summary>
